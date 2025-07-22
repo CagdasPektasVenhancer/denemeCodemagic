8ce023ea8395e28fe072973ed353be82d842d56a
çağdaş <62350321+cagdaspektas@users.noreply.github.com>
2025-07-22 16:31:33 +0300

    Update build.gradle.kts to use the new plugin syntax for Google Services and remove the deprecated buildscript section.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,13 +1,11 @@
+plugins {
+    id("com.google.gms.google-services") version "4.4.0" apply false
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
-    }
-}
-
-buildscript {
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
     }
 }
 
