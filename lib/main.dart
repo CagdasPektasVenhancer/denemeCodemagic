d90863f31256fdc40148a4679a03a99897943077
CagdasPektasVenhancer <cagdas.pektas@venhancer.com>
2025-08-02 01:00:22 +0300

    Update version in pubspec.yaml to 1.0.1+2 and adjust widget layout in main.dart for improved text alignment.

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,10 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            Center(
+              child: const Text('You have pushed the button this many times:',
+              ),
+            ),
             Text('$_counter', style: Theme.of(context).textTheme.headlineMedium),
           ],
         ),
